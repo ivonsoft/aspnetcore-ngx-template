b46768fc8b3accf4736a941cfb40d79f706c0f78
ivonsoft <szczerbowski.krzysztof@gmail.com>
2019-02-28 21:31:46 +0100

    initial commit

---
+++ DatingApp.API/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.NodeServices;
+using System.Runtime.InteropServices;
+
+namespace DatingApp.API.Controllers {
+
+   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController: ControllerBase {
+         // Insert correct filePath
+        [DllImport(@"E:\Projekty c#\WindowsNativeCpp\x64\Release\WindowsNativeCpp.dll", EntryPoint = 
+       "math_add", CallingConvention = CallingConvention.StdCall)]
+        public static extern int Add(int a, int b);
+        // GET api/Home
+        [HttpGet("Adds")]
+        public async Task < IActionResult > Adds([FromServices] INodeServices nodeServices) {
+                var num1 = 10;
+                var num2 = 20;
+                var result = await nodeServices.InvokeAsync < int > ("NodeModule\\AddModule.js", num1, num2);
+                string v = $"Result of {num1} + {num2} is {result}";
+                Console.WriteLine(v);
+                var result1 = Add(1 ,2);
+                Console.WriteLine("result of C++ is {0}", result1);
+                // ViewData["ResultFromNode"] = v;
+                return Ok(v);
+            }
+            [HttpGet("qr")]
+        public async Task < IActionResult > QR([FromServices] INodeServices nodeServices, string text = "42") {
+            var data = await nodeServices.InvokeAsync < byte[] > ("NodeModule/QR.js", text);
+            return File(data, "image/png");
+        }
+
+        [HttpGet("Preview")]
+        public async Task < IActionResult > GenerateUrlPreview([FromServices] INodeServices nodeServices) {
+            var url = Request.Form["Url"].ToString();
+            var fileName = System.IO.Path.ChangeExtension(DateTime.UtcNow.Ticks.ToString(), "png");
+            Console.WriteLine($"({DateTime.UtcNow.ToLongTimeString()}), plik: {fileName}, url: {url}");
+            var file = await nodeServices.InvokeAsync < string > ("NodeModule\\UrlPreviewModule.js", url, System.IO.Path.Combine("PreviewImages", fileName));
+
+            return Content($"<a class=\"btn btn-default\" target=\"_blank\" href=\"/Home/Download?img={fileName}\">Download image</a>");
+        }
+
+    }
+}
---
+++ DatingApp.API/documentation.md
@@ -0,0 +1,15 @@
+stages:
+0. mkdir DatingApp, then cd
+- create model, controller, Data, based on that we can create db scheleton
+1. dotnet ef migrations add initialCreate -> create migration folder
+2. dotnet ef database update -> run script and create db
+3. dotnet restore and run
+4. dodwanie dodatkowego modulu: dotnet add package Microsoft.AspNetCore.NodeServices: https://dotnetthoughts.net/using-node-services-in-aspnet-core/
+, Microsoft.EntityFrameworkCore.Sqlite
+embedded c++ dll: https://blog.quickbird.uk/calling-c-from-net-core-759563bab75d,
+https://medium.com/@xaviergeerinck/how-to-bind-c-code-with-dotnet-core-157a121c0aa6 (not tested mac)
+5. stworzenie folderu dla JS scripts: NodeModule, w tym folderze instalujesz np qr-script, node_modules, potrzebne do uruchomienia skryptow js
+
+
+
+
